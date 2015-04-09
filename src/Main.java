0cf44fc1799d0da5ce17e2962f5503fcd76f6072
Maël Valais <mael65@gmail.com>
2015-04-09 19:09:55 +0200

    Ajout d'un main

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Vector;
+
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+			List<Process> processus = new Vector<Process>();
+			List<BufferedReader> stdout = new Vector<BufferedReader>(); 
+
+			Runtime runtime = Runtime.getRuntime();
+			String command;
+			Process p;
+			ProcessBuilder pb; 
+			command = "java Noeud 0 N1";
+			pb = new ProcessBuilder(command.split(" "));
+			pb.inheritIO(); // Redirige stdout/stderr vers le processus père
+			p = pb.start();
+			stdout.add(new BufferedReader(new InputStreamReader(p.getInputStream())));
+
+			for (int i = 2; i < 5; i++) {
+				command = "java Noeud "+i*100+" N"+i +" N1";
+				pb = new ProcessBuilder(command.split(" "));
+				pb.inheritIO(); // Redirige stdout/stderr vers le processus père
+				p = pb.start();
+				processus.add(p);
+				stdout.add(new BufferedReader(new InputStreamReader(p.getInputStream())));
+			}
+
+			p.waitFor();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
