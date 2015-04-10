48e82f50b2eab4ee6aa1b1b0a0f22880487640ea
Maël Valais <mael65@gmail.com>
2015-04-10 10:52:26 +0200

    Ajout d'un main qui exécute des noeuds

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,80 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Usage: `java Main`
+ * permet de lancer plusieurs noeuds et de les insérer dans l'anneau.
+ * Ensuite, on pourra utiliser `java NoeudClient N1 get 5` pour consulter
+ * la table de hashage distribuée.
+ * @author maelv
+ *
+ */
 
 public class Main {
+	private static Process lancer(int idChord, String idRMI, String idRMIEntree) {
+		String command = "java Noeud "+idChord+" "+idRMI+" "+idRMIEntree;
+		ProcessBuilder pb = new ProcessBuilder(command.split(" "));
+		pb.inheritIO(); // Redirige stdout/stderr vers le processus père
+		try {
+			return pb.start();
+		} catch (IOException e) {
+			System.out.println("lancer("+idChord+","+idRMI+","+idRMIEntree+"):"
+					+ "erreur");
+			e.printStackTrace();
+		}
+		return null;
+	}
+	private static void get(int cle, String idRMIEntree) {
+		String command = "java NoeudClient "+idRMIEntree+" get "+cle;
+		ProcessBuilder pb = new ProcessBuilder(command.split(" "));
+		pb.inheritIO(); // Redirige stdout/stderr vers le processus père
+		try {
+			pb.start();
+		} catch (IOException e) {
+			System.out.println("put("+idRMIEntree+"):"
+					+ "erreur");
+			e.printStackTrace();
+		}
+	}
+	private static void put(int cle, int donnee, String idRMIEntree) {
+		String command = "java NoeudClient "+idRMIEntree+" put "+cle+" "+donnee;
+		ProcessBuilder pb = new ProcessBuilder(command.split(" "));
+		pb.inheritIO(); // Redirige stdout/stderr vers le processus père
+		try {
+			pb.start();
+		} catch (IOException e) {
+			System.out.println("put("+idRMIEntree+"):"
+					+ "erreur");
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		try {
-			List<Process> processus = new Vector<Process>();
-			List<BufferedReader> stdout = new Vector<BufferedReader>(); 
+			List<Process> lesprocessus = new Vector<Process>();
+			
+			Process p = Main.lancer(0,"N1","");
+			lesprocessus.add(p);
 
-			String command;
-			Process p;
-			ProcessBuilder pb; 
-			command = "java Noeud 0 N1";
-			pb = new ProcessBuilder(command.split(" "));
-			pb.inheritIO(); // Redirige stdout/stderr vers le processus père
-			p = pb.start();
-			stdout.add(new BufferedReader(new InputStreamReader(p.getInputStream())));
-
-			for (int i = 2; i < 5; i++) {
-				p.waitFor(1, TimeUnit.SECONDS);
-				command = "java Noeud "+i*100+" N"+i +" N1";
-				pb = new ProcessBuilder(command.split(" "));
-				pb.inheritIO(); // Redirige stdout/stderr vers le processus père
-				p = pb.start();
-				processus.add(p);
-				stdout.add(new BufferedReader(new InputStreamReader(p.getInputStream())));
+			List<String> noeudsNoms = new ArrayList<String>();
+			Integer[] noeudsIdChord = {6000, 4000, 1, 3000, 2999, 9000};
+			for (int i = 0; i < noeudsIdChord.length+1; i++) {
+				noeudsNoms.add("N"+(i+1));
 			}
+			
+			for (int i = 0; i < noeudsIdChord.length; i++) {
+				p.waitFor(1, TimeUnit.SECONDS);
+				lesprocessus.add(Main.lancer(noeudsIdChord[i],noeudsNoms.get(i+1),noeudsNoms.get(i)));
+			}
+			
+			get(1, "N1");
 
 			p.waitFor();
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
